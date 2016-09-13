a05058017a23d1acb70e0478119be7d835753c94
Ronald Barendse <ronald@panoramastudios.nl>
2016-09-13 15:13:27 +0200

    Edited NuGet package id and assembly info

--- MailPlus/Properties/AssemblyInfo.cs
+++ MailPlus/Properties/AssemblyInfo.cs
@@ -1,35 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("MailPlus")]
 [assembly: AssemblyDescription("The MailPlus REST API allows you to interact with your MailPlus account, giving you the opportunity to interact directly with objects such as contacts, mailings and form requests.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MailPlus")]
+[assembly: AssemblyCompany("Panorama Studios")]
 [assembly: AssemblyProduct("MailPlus REST API client")]
-[assembly: AssemblyCopyright("Panorama Studios")]
 [assembly: AssemblyTrademark("MailPlus")]
 [assembly: AssemblyCulture("en-US")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("84825d16-bedb-4f6b-bc73-a8cacf1fe121")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
